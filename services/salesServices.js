22372922b0582fb11e7a9c84bc2c2777c8f45bab
Lucas Pimentel <lucas.mirandapimentel@gmail.com>
2022-06-03 23:16:08 +0000

    (fix) lint

--- services/salesServices.js
+++ services/salesServices.js
@@ -6,16 +6,16 @@ const getAllSales = async () => {
   
   if (!sales) throw new CustomError(404, 'Sales Not Found');
   return sales;
-}
+};
 
 const getSaleByID = async (id) => {
   const [sale] = await salesModels.getSaleByID(id);
   
   if (!sale) throw new CustomError(404, 'Sale not found');
   return sale;
-}
+};
 
 module.exports = {
   getAllSales,
   getSaleByID,
-}
+};
